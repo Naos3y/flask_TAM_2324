7796e93dec7b014b63a4926a76fa92a9934dfd6b
Naos3y <93429100+Naos3y@users.noreply.github.com>
2024-01-02 18:16:15 +0000

    login e token

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 import psycopg2
 import jwt
-import datetime
+from datetime import datetime, timedelta
 import os
+from functools import wraps
 
 app = Flask(__name__)
 app.debug = True
@@ -11,13 +12,78 @@ host = os.environ.get("host")
 dbname = os.environ.get("dbname")
 user = os.environ.get("user")
 password = os.environ.get("password")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# os erros 400 e 201 indicam que exisiu um erro (bad request) ou que algo foi criado, respetivamente
-# o 200 indica que esta tudo bem
+NOT_FOUND_CODE = 400
+OK_CODE = 200
+SUCCESS_CODE = 201
+BAD_REQUEST_CODE = 400
+UNAUTHORIZED_CODE = 401
+FORBIDDEN_CODE = 403
+NOT_FOUND = 404
+SERVER_ERROR = 500
 
 
 def connect_to_db():
     return psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+
+
+def auth_user(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.headers.get("Authorization")
+
+        if not token:
+            return jsonify({"Erro": "Token está em falta!", "Code": UNAUTHORIZED_CODE})
+
+        try:
+            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+
+            if data["expiration"] < str(datetime.utcnow()):
+                return jsonify({"Erro": "O Token expirou!"}), NOT_FOUND_CODE
+
+        except Exception as e:
+            return jsonify({"Erro": "Token inválido"}), FORBIDDEN_CODE
+
+        return func(*args, **kwargs)
+
+    return decorated
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    u_username = data.get("u_username")
+    u_password = data.get("u_password")
+
+    if "u_username" not in data or "u_password" not in data:
+        return jsonify({"Code": BAD_REQUEST_CODE, "Erro": "Parâmetros inválidos"})
+
+    try:
+        conn = connect_to_db()
+        cursor = conn.cursor()
+
+        cursor.callproc("mydbtam.verifica_login", (u_username, u_password))
+        result = cursor.fetchone()[0]
+
+        cursor.close()
+        conn.close()
+
+        if result:
+            token = jwt.encode(
+                {
+                    "username": u_username,
+                    "expiration": str(datetime.utcnow() + timedelta(hours=1)),
+                },
+                SECRET_KEY,
+                algorithm="HS256",
+            )
+            return jsonify({"access_token": token})
+        else:
+            return jsonify({"Erro": "Credenciais inválidas"})
+
+    except Exception as e:
+        return jsonify({"Code": 500, "Erro": str(e)})
 
 
 @app.route("/register", methods=["POST"])
@@ -43,17 +109,17 @@ def register_user():
         conn.close()
 
         if result == 0:
-            return jsonify({"error": "Email ou username já existem."}), 400
+            return jsonify({"error": "Email ou username já existem."}), NOT_FOUND_CODE
         else:
             return (
                 jsonify(
                     {"message": "Utilizador registado com sucesso!", "user_id": result}
                 ),
-                201,
+                SUCCESS_CODE,
             )
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)}), NOT_FOUND_CODE
 
 
 @app.route("/users", methods=["GET"])
@@ -79,10 +145,10 @@ def get_all_users():
         cursor.close()
         conn.close()
 
-        return jsonify(users), 200
+        return jsonify(users), OK_CODE
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": str(e)}), NOT_FOUND_CODE
 
 
 if __name__ == "__main__":
